1cf6501d4c77fccd0153d226f1e23cdb21bea635
unknown <teraco@gmail.com>
2019-08-18 08:28:27 +0900

    add hoge

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Hoge from '@/components/Hoge';
 
 Vue.use(Router);
 
@@ -11,5 +12,10 @@ export default new Router({
       name: 'HelloWorld',
       component: HelloWorld,
     },
+    {
+      path: '/hoge',
+      name: 'Hoge',
+      component: Hoge,
+    }
   ],
 });
