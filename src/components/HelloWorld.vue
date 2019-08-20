b79c28821aef80c2d338cf697c9f7470dd1d4ccc
unknown <teraco@gmail.com>
2019-08-20 22:01:02 +0900

    final commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,16 +2,20 @@
   <div class="hello">
     {{msg}}
     <form>
-      <button>ADD TASK</button>
-      <button>DELETE FINISHED TASK</button>
-      <p>input: <input type="text"></p>
-      <p>task:</p>
+      <button @click="addTodo()">ADD TASK</button>
+      <button @click="removeTodo()">DELETE FINISHED TASK</button>
+      <p>input: <input type="text" v-model="newTodo"></p>
+      <p>task:{{newTodo}}</p>
     </form>
     <div class="task-list">
-      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vue-router</label>
-      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vuex</label>
-      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vue-loader</label>
-      <label class="task-list__item--checked"><input type="checkbox" checked><button>EDIT</button>awesome-vue</label>
+      <label class="task-list__item"
+             v-for="todo in todos"
+             v-bind:class="{ 'task-list__item--checked': todo.done }">
+        <input type="checkbox" v-model="todo.done">
+        <input type="checkbox" v-model="todo.editing">
+        <input v-if="todo.editing" v-model="todo.text" @keyup.enter="todo.editing = !todo.editing">
+        <span v-else>{{ todo.text }}</span>
+      </label>
     </div>
   </div>
 </template>
@@ -21,8 +25,34 @@ export default {
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
+      todos : [
+        {text : 'vue-router', done: false},
+        {text : 'vuex', done: false},
+        {text : 'vue-loader', done: false},
+        {text : 'awesome-vue', done: true },
+      ],
+      newTodo: ""
     };
   },
+  methods: {
+    addTodo: function(event) {
+      let text = this.newTodo && this.newTodo.trim()
+      if (!text) {
+        return
+      }
+      this.todos.push({
+        text: text,
+        done: false,
+        editing: false
+      })
+      this.newTodo = ''
+    },
+    removeTodo: function (event) {
+      for (let i = this.todos.length - 1; i >= 0; i--) {
+        if (this.todos[i].done) this.todos.splice(i, 1)
+      }
+    }
+  }
 };
 </script>
 
